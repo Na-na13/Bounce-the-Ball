1f392681a85de5cb67a6485a2b63a7b59066de8f
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2021-11-15 23:15:32 +0200

    maksukortin testit tehty

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -3,7 +3,30 @@ from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
-        self.maksukortti = Maksukortti(10)
+        self.maksukortti = Maksukortti(1000)
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 10.0")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "saldo: 15.0")
+
+    def test_saldo_vahenee_oikein_jos_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "saldo: 5.0")
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(2000)
+        self.assertEqual(str(self.maksukortti), "saldo: 10.0")
+
+    def test_rahat_riittivat(self):
+        message = "virhe: rahojen olisi pitänyt riittää"
+        self.assertTrue(self.maksukortti.ota_rahaa(500),message)
+    
+    def test_rahat_eivat_riittaneet(self):
+        message = "virhe: rahojen ei olisi pitänyt riittää"
+        self.assertFalse(self.maksukortti.ota_rahaa(2000),message)
