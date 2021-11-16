1a2f062b09f7769c0c6cd169e3a8e8127bbb2c84
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2021-11-16 17:52:52 +0200

    kassapäätetestejä paranneltu

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -25,10 +25,6 @@ class TestKassapaate(unittest.TestCase):
         self.assertEqual(takaisin, 150)
         self.assertEqual(self.kassapaate.edulliset, 0)
 
-#    def test_kateismaksu_riittaa_lounaiden_maara_kasvaa_edullinen(self):
-#        self.kassapaate.syo_edullisesti_kateisella(240)
-#        self.assertEqual(self.kassapaate.edulliset, 1)
-
     def test_kateismaksu_riittaa_rahastus_oikein_maukas(self):
         takaisin = self.kassapaate.syo_maukkaasti_kateisella(460)
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100400) 
@@ -41,19 +37,11 @@ class TestKassapaate(unittest.TestCase):
         self.assertEqual(takaisin, 150)
         self.assertEqual(self.kassapaate.maukkaat, 0)
 
-#    def test_kateismaksu_riittaa_lounaiden_maara_kasvaa_maukas(self):
-#        self.kassapaate.syo_maukkaasti_kateisella(400)
-#        self.assertEqual(self.kassapaate.maukkaat, 1)
-
     def test_kortilla_riittavasti_rahaa_edullinen(self):
         ret = self.kassapaate.syo_edullisesti_kortilla(self.maksukortti)
         self.assertEqual(ret, True)
         self.assertEqual(self.maksukortti.saldo, 760)
         self.assertEqual(self.kassapaate.edulliset, 1)
-
-#    def test_korttimaksu_riittaa_lounaiden_maara_kasvaa_edullinen(self):
-#        self.kassapaate.syo_edullisesti_kortilla(self.maksukortti)
-#        self.assertEqual(self.kassapaate.edulliset, 1)
 
     def test_korttimaksu_ei_riita_edullinen(self):
         ret = self.kassapaate.syo_edullisesti_kortilla(self.maksukortti_vajaa)
@@ -61,14 +49,6 @@ class TestKassapaate(unittest.TestCase):
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
         self.assertEqual(self.kassapaate.edulliset,0)
         self.assertEqual(self.maksukortti_vajaa.saldo, 200)
-        
-#    def test_korttimaksu_ei_riita_kassan_rahamaara_ei_muutu_edullinen(self): 
-#        ret = self.kassapaate.syo_edullisesti_kortilla(self.maksukortti_vajaa)
-#        self.assertEqual((self.kassapaate.kassassa_rahaa,ret),(100000,False))
-    
-#    def test_korttimaksu_ei_riita_kortin_saldo_ei_muutu_edullinen(self):
-#        self.kassapaate.syo_edullisesti_kortilla(self.maksukortti_vajaa)
-#        self.assertEqual(self.maksukortti_vajaa.saldo,200)
 
     def test_kortilla_riittavasti_rahaa_maukas(self):
         ret = self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti)
@@ -76,28 +56,12 @@ class TestKassapaate(unittest.TestCase):
         self.assertEqual(self.maksukortti.saldo, 600)
         self.assertEqual(self.kassapaate.maukkaat, 1)
 
-#    def test_korttimaksu_riittaa_lounaiden_maara_kasvaa_maukas(self):
-#        self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti)
-#        self.assertEqual(self.kassapaate.maukkaat, 1)
-
     def test_korttimaksu_ei_riita_maukas(self):
         ret = self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti_vajaa)
         self.assertEqual(ret, False)
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
         self.assertEqual(self.kassapaate.maukkaat, 0)
         self.assertEqual(self.maksukortti_vajaa.saldo, 200)
-
-#    def test_korttimaksu_ei_riita_lounaiden_maara_ei_muutu_maukas(self):
-#        self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti_vajaa)
-#        self.assertEqual(self.kassapaate.maukkaat,0)
-        
-#    def test_korttimaksu_ei_riita_kassan_rahamaara_ei_muutu_maukas(self): 
-#        ret = self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti_vajaa)
-#        self.assertEqual((self.kassapaate.kassassa_rahaa,ret),(100000,False))
-
-#    def test_korttimaksu_ei_riita_kortin_saldo_ei_muutu_maukas(self):
-#        self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti_vajaa)
-#        self.assertEqual(self.maksukortti_vajaa.saldo,200)
 
     def test_kortilla_lataaminen_toimii_kun_summa_oikea(self):
         self.kassapaate.lataa_rahaa_kortille(self.maksukortti,500)
