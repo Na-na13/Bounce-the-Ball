f91814979458dbd3b973e0a5aa3d042e4a95b21e
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2021-11-28 17:39:08 +0200

    clock lisätty

---
+++ src/game/clock.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Clock:
+    def __init__(self):
+        self.clock = pygame.time.Clock()
+
+    def clock_tick(self, fps):
+        self.clock.tick(fps)
+
+    def clock_get_ticks(self):
+        return pygame.time.get_ticks()
