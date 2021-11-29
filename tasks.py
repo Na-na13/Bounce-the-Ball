6c111f212c49113906b71b141e830c2304b3ce34
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2021-11-29 15:25:17 +0200

    invoke-tehtäviä lisätty

--- tasks.py
+++ tasks.py
@@ -11,3 +11,13 @@ def pylint(ctx):
 @task
 def test(ctx):
     ctx.run("pytest src")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage html")
+
+
