5253069a536713d9a371c296751dd9029c0c2a59
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2021-12-26 22:10:03 +0200

    settings-lista lisätty

---
+++ src/settings.py
@@ -0,0 +1,17 @@
+# colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+
+# game settings
+MARGIN = 20
+LINE = 2
+MAXJUMPHEIGHT = 150
+BALLRADIUS = 20
+STARW = 35
+STARH = 34
+
+# menu settings
+BUTTONW = 200
+BUTTONH = 40
+NAMELENGTH = 9
