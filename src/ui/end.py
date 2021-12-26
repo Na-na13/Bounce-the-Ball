d139a9088648823b0e4cf7782758374fc8b393d8
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2021-12-26 18:46:59 +0200

    tiedostosta haku toteutettu

--- src/ui/end.py
+++ src/ui/end.py
@@ -13,48 +13,49 @@ class End:
         self.background = pygame.Surface((self.gamewindow.width, self.gamewindow.height))
         self.manager = pygame_gui.UIManager((self.gamewindow.width, self.gamewindow.height))
         self.save_panel = pygame_gui.elements.UIPanel(pygame.Rect(self.gamewindow.width/2-125,
-                                                self.gamewindow.height/2-100,
+                                                self.gamewindow.height/2-60,
                                                 250, 165),
                                                 starting_layer_height=1,
                                                 manager=self.manager,)
-        self.text = pygame_gui.elements.UITextBox(relative_rect=pygame.Rect(
-                                                (2, 0),
-                                                (240, 93)),
-                                                html_text= "<b>Well played!</b>"
-                                                "<br><b>Your time: {time} sec!</b>"
-                                                "<br>Write your name below to SAVE your TIME."
-                                                .format(time=self.time),
-                                                manager=self.manager,
-                                                container=self.save_panel)
+        self.save_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(
+                                                        (77,118),
+                                                        (100, 40)),
+                                                        text="Save",
+                                                        visible=False,
+                                                        manager=self.manager,
+                                                        container=self.save_panel)
         self.nick_entry = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(
                                                     (32,91),
                                                     (190,50)),
                                                     manager=self.manager,
+                                                    visible=False,
                                                     container=self.save_panel)
-        self.savescore_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(
-                                                        (77,118),
-                                                        (100, 40)),
-                                                        text="Save",
-                                                        manager=self.manager,
-                                                        container=self.save_panel)
+        self.nick_entry.set_text_length_limit(9)
+        self.text = pygame_gui.elements.UITextBox(relative_rect=pygame.Rect(
+                                                (2, 0),
+                                                (240, 93)),
+                                                html_text=self.get_text(self.time),
+                                                manager=self.manager,
+                                                container=self.save_panel)
         self.newgame_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(
-                                                        self.gamewindow.width/2-100,
-                                                        self.gamewindow.height/2+70,
-                                                        200, 40),
-                                                        text="New Game",
-                                                        manager=self.manager)
-        #self.score_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(
-        #                                                self.gamewindow.width/2-100,
-        #                                                self.gamewindow.height/2+70,
-        #                                                200, 40),
-        #                                                text="High Score List",
-        #                                                manager=self.manager)
-        self.quit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(
                                                         self.gamewindow.width/2-100,
                                                         self.gamewindow.height/2+115,
                                                         200, 40),
+                                                        text="New Game",
+                                                        manager=self.manager)
+        self.quit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(
+                                                        self.gamewindow.width/2-100,
+                                                        self.gamewindow.height/2+160,
+                                                        200, 40),
                                                         text="Quit",
                                                         manager=self.manager)
+        self.score_list = pygame_gui.elements.UITextBox(relative_rect=pygame.Rect(
+                                                    self.gamewindow.width/2-100,
+                                                    self.gamewindow.height/2-115,
+                                                    200, 225),
+                                                    html_text= "<b>Text</b>",
+                                                    manager=self.manager,
+                                                    visible=False)
 
     def run(self):
         is_running = True
@@ -70,8 +71,10 @@ class End:
                         if event.ui_element == self.quit_button:
                             is_running = False
                             sys.exit()
-                        #if event.ui_element == self.save_button:
-                        #    save scores to file
+                        if event.ui_element == self.save_button:
+                            nick = self.nick_entry.get_text().strip()
+                            if  nick != "":
+                                self.save(nick, self.time)
 
                 self.manager.process_events(event)
 
@@ -80,3 +83,55 @@ class End:
             self.manager.draw_ui(self.gamewindow.display)
             self.gameclock.clock_tick(60)
             pygame.display.update()
+
+    def save(self, name, gametime):
+        scores = []
+        with open("high_score.txt","r") as file:
+            for line in file:
+                if "\n" in line or line == "":
+                    continue
+                parts = line.split(";")
+                nick = str(parts[0])
+                time = float(parts[1])
+                scores.append((nick,time))
+        if float(gametime) < float(scores[-1][1]):
+            scores.remove(scores[-1])
+            scores.append((name,float(gametime)))
+            scores.sort(key=lambda x:x[1])
+            with open("high_score.txt","w") as file:
+                for i in range(0,len(scores)):
+                    file.write(f"{str(scores[i][0])};{float(scores[i][1])}\n")
+        self.save_panel.kill()
+        scorelist = "<b>Best Times</b>"
+        i = 1
+
+        for score in scores:
+            scorelist = scorelist + f"<br><b>{i}: </b>{score[0]}: {score[1]}"
+            i += 1
+        score_list = pygame_gui.elements.UITextBox(relative_rect=pygame.Rect(
+                                            self.gamewindow.width/2-100,
+                                            self.gamewindow.height/2-115,
+                                            200, 225),
+                                            html_text= scorelist,
+                                            manager=self.manager,)
+
+    def get_text(self,playtime):
+        scores = []
+        with open("high_score.txt","r") as file:
+            for line in file:
+                if line == "\n" or line == "":
+                    continue
+                parts = line.split(";")
+                nick = str(parts[0])
+                time = parts[1]
+                scores.append((nick,time))
+
+        if playtime < scores[-1][1]:
+            self.save_button.visible = True
+            self.nick_entry.visible = True
+            txt = f"<b>Well played!</b><br><b>Your time: {playtime} sec!</b><br>Write your name below to SAVE your TIME."
+            return txt
+        self.save_button.disable()
+        self.nick_entry.disable()
+        txt = f"<b>Well played!</b><br><b>Your time: {playtime} sec!</b>"
+        return txt
--- src/ui/start.py
+++ src/ui/start.py
@@ -31,7 +31,13 @@ class Start:
                                                         200, 40),
                                                         text="Best times",
                                                         manager=self.manager)
-
+        self.return_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(
+                                                        self.gamewindow.width/2-100,
+                                                        self.gamewindow.height/2+160,
+                                                        200, 40),
+                                                        text="Return",
+                                                        manager=self.manager,
+                                                        visible=False)
         self.quit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(
                                                         self.gamewindow.width/2-100,
                                                         self.gamewindow.height/2+115,
@@ -50,13 +56,13 @@ class Start:
                                             "<br>Press Q to exit game.",
                                                     manager=self.manager,
                                                     visible=False)
-        self.return_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(
-                                                        self.gamewindow.width/2-100,
-                                                        self.gamewindow.height/2+160,
-                                                        200, 40),
-                                                        text="Return",
-                                                        manager=self.manager,
-                                                        visible=False)
+        self.score_list = pygame_gui.elements.UITextBox(relative_rect=pygame.Rect(
+                                                    self.gamewindow.width/2-100,
+                                                    self.gamewindow.height/2-70,
+                                                    200, 225),
+                                                    html_text= self.get_scorelist(),
+                                                    manager=self.manager,
+                                                    visible=False)
 
     def run(self):
         is_running = True
@@ -74,19 +80,14 @@ class Start:
                             is_running = False
                             sys.exit()
                         if event.ui_element == self.inst_button:
-                            self.inst_text.visible = True
-                            self.return_button.visible = True
-                            self.play_button.disable()
-                            self.score_button.disable()
-                            self.inst_button.disable()
-                            self.quit_button.disable()
+                            self.show_textbox(self.inst_text)
                         if event.ui_element == self.return_button:
-                            self.inst_text.visible = False
-                            self.return_button.visible = False
-                            self.play_button.enable()
-                            self.score_button.enable()
-                            self.inst_button.enable()
-                            self.quit_button.enable()
+                            if self.inst_text.visible is True:
+                                self.hide_textbox(self.inst_text)
+                            if self.score_list.visible is True:
+                                self.hide_textbox(self.score_list)
+                        if event.ui_element == self.score_button:
+                            self.show_textbox(self.score_list)
 
                 self.manager.process_events(event)
 
@@ -95,4 +96,38 @@ class Start:
             self.manager.draw_ui(self.gamewindow.display)
             self.gameclock.clock_tick(60)
             pygame.display.update()
-        
+
+    def show_textbox(self, textbox):
+        textbox.visible = True
+        self.return_button.visible = True
+        self.play_button.disable()
+        self.score_button.disable()
+        self.inst_button.disable()
+        self.quit_button.disable()
+
+    def hide_textbox(self, textbox):
+        textbox.visible = False
+        self.return_button.visible = False
+        self.play_button.enable()
+        self.score_button.enable()
+        self.inst_button.enable()
+        self.quit_button.enable()
+
+    def get_scorelist(self):
+        with open("high_score.txt","r") as file:
+            scores = []
+            for line in file:
+                if line == "\n" or line == "":
+                    continue
+                parts = line.split(";")
+                nick = parts[0]
+                time = parts[1].strip()
+                scores.append((nick,time))
+
+        scorelist = "<b>Best Times</b>"
+        i = 1
+
+        for score in scores:
+            scorelist = scorelist + f"<br><b>{i}: </b>{score[0]}: {score[1]}"
+            i += 1
+        return scorelist
